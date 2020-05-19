dff2c2b99a535e3b2acaf2193173cfe6e6b2ede1
jkaram <jamiekaram16@gmail.com>
2020-05-19 11:19:23 -0400

    list blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,17 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+require("dotenv").config()
+
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `g9kyijm6ddk3`,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,29 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+export const query = graphql`
+  {
+    posts: allContentfulPost {
+      nodes {
+        title
+        slug
+        createdAt
+        image {
+          fluid {
+            src
+          }
+        }
+      }
+    }
+  }
+`
+
+export default ({ data }) => (
+  <div>
+    {data.posts.nodes.map(post => (
+      <p>
+        <Link to={`/${post.slug}`}>{post.title}</Link>
+      </p>
+    ))}
+  </div>
+)
