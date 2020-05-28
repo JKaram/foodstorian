8393a0c34acf2950713e25e2491bb9a114e7a9ac
jkaram <jamiekaram16@gmail.com>
2020-05-27 22:27:28 -0400

    new update

--- src/components/blogItem.js
+++ src/components/blogItem.js
@@ -1,7 +1,6 @@
 import React from "react"
 import styled from "styled-components"
 import { StyledLink } from "./index"
-import flag from "../../static/images/flags/china.svg"
 
 export const BlogItem = ({
   slug,
