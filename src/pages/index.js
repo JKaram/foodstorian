62b0d141d81d44026c2d4cf2c3b79d7cc53198e3
jkaram <jamiekaram16@gmail.com>
2020-05-28 21:53:00 -0400

    add redirect to /blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,49 +1,9 @@
-import React from "react"
-import { graphql } from "gatsby"
+import { useEffect } from "react"
+import { navigate } from "gatsby"
 
-import { Layout, AboutSection, BlogItem } from "../components/index"
-
-export default ({ data }) => (
-  <Layout>
-    <AboutSection />
-    <div style={{ display: "flex", flexDirection: "column", margin: "0 auto" }}>
-      {data.posts.nodes.map(post => {
-        const description =
-          post.description !== null ? post.description.description : null
-        return (
-          <BlogItem
-            key={post.slug}
-            slug={post.slug}
-            image={post.image.resize.src}
-            title={post.title}
-            createdAt={post.createdAt}
-            country={post.country}
-            description={description}
-          />
-        )
-      })}
-    </div>
-  </Layout>
-)
-
-export const query = graphql`
-  {
-    posts: allContentfulPost(sort: { fields: [createdAt], order: DESC }) {
-      nodes {
-        title
-        slug
-        country
-        createdAt(formatString: "DD MMMM YYYY")
-        description {
-          description
-        }
-        image {
-          resize(width: 640, height: 400) {
-            width
-            src
-          }
-        }
-      }
-    }
-  }
-`
+export default () => {
+  useEffect(() => {
+    navigate("/blog")
+  }, [])
+  return null
+}
