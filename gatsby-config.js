3c82b9d1dafebe3324da2f963a2a133da06ac645
jkaram <jamiekaram16@gmail.com>
2020-06-03 15:54:06 -0400

    cleanup plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,14 +17,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-plugin-algolia`,
-      options: {
-        appId: process.env.GATSBY_ALGOLIA_APP_ID,
-        apiKey: process.env.ALGOLIA_ADMIN_KEY,
-        chunkSize: 10000, // default: 1000
-      },
-    },
-    {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: `g9kyijm6ddk3`,
